0414046792e21ffcdc4a2b4466771efdd3e93f96
berto9675 <albrtvd@gmail.com>
2025-03-29 20:37:30 +0100

    finished the tests

--- src/test/java/dev/berto/loops/AppTest.java
+++ src/test/java/dev/berto/loops/AppTest.java
@@ -3,16 +3,38 @@ package dev.berto.loops;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
-/**
- * Unit test for simple App.
- */
+import org.junit.jupiter.api.DisplayName;
+
+
 class AppTest {
-    /**
-     * Rigorous Test.
-     */
+    
     @Test
+    @DisplayName("Test when displaying the multiplication table in the console")
     void testApp() {
-        assertEquals(1, 1);
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+
+        App.main(new String[]{});
+
+        String [] currentOutput = outContent.toString().split(System.lineSeparator());
+        String [] expectedOutput = {
+            "5 x 1 = 5",
+            "5 x 2 = 10",
+            "5 x 3 = 15",
+            "5 x 4 = 20",
+            "5 x 5 = 25",
+            "5 x 6 = 30",
+            "5 x 7 = 35",
+            "5 x 8 = 40",
+            "5 x 9 = 45",
+            "5 x 10 = 50"
+        };
+
+        
+        assertEquals(expectedOutput, currentOutput);
+        
     }
 }
